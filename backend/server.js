673b220511c80fe609f026d7b79e4c9a0cbc20c9
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-08-31 13:40:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('files'))
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use('/api/videos', videoRoutes);
 
